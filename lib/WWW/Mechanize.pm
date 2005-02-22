2809acdb79dc709879a96d5ad5d318ce544a1acb
Andy Lester <andy@petdance.com>
2005-02-22 04:02:51 +0000

    Releasing 1.11_02

--- lib/WWW/Mechanize.pm
+++ lib/WWW/Mechanize.pm
@@ -6,11 +6,11 @@ WWW::Mechanize - Handy web browsing in a Perl object
 
 =head1 VERSION
 
-Version 1.11_01
+Version 1.11_02
 
 =cut
 
-our $VERSION = "1.11_01";
+our $VERSION = "1.11_02";
 
 =head1 SYNOPSIS
 
