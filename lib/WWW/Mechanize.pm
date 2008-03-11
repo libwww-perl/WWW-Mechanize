9d27d74e1ec062b38301ebf1aa00a4338798f27b
Andy Lester <andy@petdance.com>
2008-03-11 06:07:30 +0000

    releasing 1.36

--- lib/WWW/Mechanize.pm
+++ lib/WWW/Mechanize.pm
@@ -6,11 +6,11 @@ WWW::Mechanize - Handy web browsing in a Perl object
 
 =head1 VERSION
 
-Version 1.35_01
+Version 1.36
 
 =cut
 
-our $VERSION = '1.35_01';
+our $VERSION = '1.36';
 
 =head1 SYNOPSIS
 
@@ -2733,7 +2733,7 @@ and the late great Iain Truskett.
 
 =head1 COPYRIGHT
 
-Copyright (c) 2005-2007 Andy Lester. All rights reserved. This program is
+Copyright (c) 2005-2008 Andy Lester. All rights reserved. This program is
 free software; you can redistribute it and/or modify it under the same
 terms as Perl itself.
 
