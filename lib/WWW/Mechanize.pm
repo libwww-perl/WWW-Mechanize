1e0115533bc054e5f54e51f6ea390f2bb7321cd8
Andy Lester <andy@petdance.com>
2008-09-28 04:51:08 +0000

    Rleeasing 1.49_01

--- lib/WWW/Mechanize.pm
+++ lib/WWW/Mechanize.pm
@@ -6,11 +6,11 @@ WWW::Mechanize - Handy web browsing in a Perl object
 
 =head1 VERSION
 
-Version 1.36
+Version 1.49_01
 
 =cut
 
-our $VERSION = '1.36';
+our $VERSION = '1.49_01';
 
 =head1 SYNOPSIS
 
