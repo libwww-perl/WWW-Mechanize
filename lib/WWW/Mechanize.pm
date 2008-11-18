1c05171079458096ae1341d0a16608b7f87e0ae0
Andy Lester <andy@petdance.com>
2008-11-18 05:51:41 +0000

    updating versions

--- lib/WWW/Mechanize.pm
+++ lib/WWW/Mechanize.pm
@@ -6,11 +6,11 @@ WWW::Mechanize - Handy web browsing in a Perl object
 
 =head1 VERSION
 
-Version 1.51_01
+Version 1.51_02
 
 =cut
 
-our $VERSION = '1.51_01';
+our $VERSION = '1.51_02';
 
 =head1 SYNOPSIS
 
