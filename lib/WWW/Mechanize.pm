0c997d057fd15d7d7d8ed3a4d0f963cfc25181af
Andy Lester <andy@petdance.com>
2009-06-23 21:59:21 +0000

    packaging for 1.55_01

--- lib/WWW/Mechanize.pm
+++ lib/WWW/Mechanize.pm
@@ -6,11 +6,11 @@ WWW::Mechanize - Handy web browsing in a Perl object
 
 =head1 VERSION
 
-Version 1.52
+Version 1.55_01
 
 =cut
 
-our $VERSION = '1.52';
+our $VERSION = '1.55_01';
 
 =head1 SYNOPSIS
 
