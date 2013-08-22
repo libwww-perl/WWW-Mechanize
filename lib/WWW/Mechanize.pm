7fd5e29e7439452be5bd336ae09c4c11e189c440
Gabor Szabo <szabgab@gmail.com>
2013-08-22 20:53:24 +0300

    another example for follow_link

--- lib/WWW/Mechanize.pm
+++ lib/WWW/Mechanize.pm
@@ -754,6 +754,14 @@ or
 
     $mech->follow_link( n => 3 );
 
+=item the link with the url
+
+    $mech->follow_link( url => '/other/page' );
+
+or
+
+    $mech->follow_link( url => 'http://example.com/page' );
+
 =back
 
 Returns the result of the GET method (an HTTP::Response object) if
