c752e2502eaf3c7534c76e3479c736d03b5a4a8d
Olaf Alders <olaf@wundersolutions.com>
2016-08-05 12:35:10 -0400

    Bumps version to 1.77

--- lib/WWW/Mechanize.pm
+++ lib/WWW/Mechanize.pm
@@ -119,7 +119,7 @@ Frequently asked questions.  Make sure you read here FIRST.
 use strict;
 use warnings;
 
-our $VERSION = 1.76;
+our $VERSION = 1.77;
 
 use HTTP::Request 1.30;
 use LWP::UserAgent 5.827;
