c0eec701519e99aabce16be9e0f67405eaf06f2e
Olaf Alders <olaf@wundersolutions.com>
2018-10-18 15:55:44 -0400

    increment $VERSION after 1.89 release

--- lib/WWW/Mechanize.pm
+++ lib/WWW/Mechanize.pm
@@ -133,7 +133,7 @@ Frequently asked questions.  Make sure you read here FIRST.
 use strict;
 use warnings;
 
-our $VERSION = '1.89';
+our $VERSION = '1.90';
 
 use Tie::RefHash;
 use HTTP::Request 1.30;
--- lib/WWW/Mechanize/Image.pm
+++ lib/WWW/Mechanize/Image.pm
@@ -3,7 +3,7 @@ package WWW::Mechanize::Image;
 use strict;
 use warnings;
 
-our $VERSION = '1.89';
+our $VERSION = '1.90';
 
 #ABSTRACT: Image object for WWW::Mechanize
 
