e5f6e050915aa8443368557292084a8129ba4312
Olaf Alders <olaf@wundersolutions.com>
2021-08-06 08:34:16 -0400

    increment $VERSION after 2.04 release

--- lib/WWW/Mechanize.pm
+++ lib/WWW/Mechanize.pm
@@ -133,7 +133,7 @@ Frequently asked questions.  Make sure you read here FIRST.
 use strict;
 use warnings;
 
-our $VERSION = '2.04';
+our $VERSION = '2.05';
 
 use Tie::RefHash;
 use HTTP::Request 1.30;
--- lib/WWW/Mechanize/Link.pm
+++ lib/WWW/Mechanize/Link.pm
@@ -3,7 +3,7 @@ package WWW::Mechanize::Link;
 use strict;
 use warnings;
 
-our $VERSION = '2.04';
+our $VERSION = '2.05';
 
 #ABSTRACT: Link object for WWW::Mechanize
 
