692c053939e4515badff0c2ac519ad893b48d9e6
Julien Fiegehenn <simbabque@cpan.org>
2022-08-15 20:26:01 +0100

    increment $VERSION after 2.14 release

--- lib/WWW/Mechanize/Image.pm
+++ lib/WWW/Mechanize/Image.pm
@@ -3,7 +3,7 @@ package WWW::Mechanize::Image;
 use strict;
 use warnings;
 
-our $VERSION = '2.14';
+our $VERSION = '2.15';
 
 #ABSTRACT: Image object for WWW::Mechanize
 
--- lib/WWW/Mechanize/Link.pm
+++ lib/WWW/Mechanize/Link.pm
@@ -3,7 +3,7 @@ package WWW::Mechanize::Link;
 use strict;
 use warnings;
 
-our $VERSION = '2.14';
+our $VERSION = '2.15';
 
 #ABSTRACT: Link object for WWW::Mechanize
 
