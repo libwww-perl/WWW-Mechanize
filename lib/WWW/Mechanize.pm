d09c251762b318434aa066ab198e6c4ad27cef04
Mikko Johannes Koivunalho <mikko.koivunalho@iki.fi>
2025-10-10 23:41:11 +0200

    Tidify lib/WWW/Mechanize.pm

--- lib/WWW/Mechanize.pm
+++ lib/WWW/Mechanize.pm
@@ -1715,10 +1715,9 @@ sub form_action {
     my ( $self, $action ) = @_;
 
     my $temp;
-    my @matches = grep {
-        defined( $temp = $_->action )
-            and ( $temp =~ m/$action/msx )
-    } $self->forms;
+    my @matches
+        = grep { defined( $temp = $_->action ) and ( $temp =~ m/$action/msx ) }
+        $self->forms;
 
     my $nmatches = @matches;
     if ( $nmatches > 0 ) {
